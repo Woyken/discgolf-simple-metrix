d733e361319e6b5c5410746613e4e6b7d960c669
Woyken <13166802+Woyken@users.noreply.github.com>
2024-12-09 17:43:21 +0200

    Add navigation solid-ui

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,27 @@
-// import { useLocation } from "@solidjs/router";
-
 import { A, useLocation } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
 import { createMemo, Show, Suspense } from "solid-js";
 import { discGolfMetrixGetAccountSettings } from "~/apiWrapper/getAccountSettings";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuDescription,
+  NavigationMenuIcon,
+  NavigationMenuItem,
+  NavigationMenuLabel,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const useSettingsQuery = () => {
   return createQuery(() => ({
@@ -26,101 +44,159 @@ export default function NavHandleLogin() {
 }
 
 function Nav() {
-  const settingsQuery = useSettingsQuery();
   return (
-    <div class="navbar bg-base-100">
-      <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block h-5 w-5 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <div class="flex-1">
-        <A href="/" class="btn btn-ghost text-xl">
+    <div class="flex items-center justify-between mx-auto px-4 py-4">
+      <NavigationMenu>
+        <NavigationMenuTrigger as={A} href="/">
           Disc Golf Metrix
-        </A>
-      </div>
-      <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block h-5 w-5 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
-        <div class="dropdown dropdown-end">
-          <div
-            tabindex="0"
-            role="button"
-            class="btn btn-ghost btn-circle avatar placeholder"
-          >
-            <div class="w-10 rounded-full">
-              <Suspense
-                fallback={<span class="loading loading-ring loading-lg" />}
-              >
-                <Show
-                  when={settingsQuery.data?.imageUrl}
-                  fallback={
-                    <span>
-                      {settingsQuery.data?.firstName?.[0].toUpperCase() ?? "F"}
-                      {settingsQuery.data?.lastName?.[0].toUpperCase() ?? "L"}
-                    </span>
-                  }
-                >
-                  {(url) => <img alt="User avatar" src={url().href} />}
-                </Show>
-              </Suspense>
-            </div>
-          </div>
-          <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <A href="/profile">Profile</A>
-            </li>
-            <li>
-              <A href="/settings">Settings</A>
-            </li>
-            <li>
-              <A href="/logout">Logout</A>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+        </NavigationMenuTrigger>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            Getting started
+            <NavigationMenuIcon />
+          </NavigationMenuTrigger>
 
-    // <nav class="navbar bg-base-100">
-    //   <ul class="container flex items-center p-3 text-gray-200">
-    //     <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-    //       <a href="/">Home</a>
-    //     </li>
-    //     <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-    //       <a href="/about">About</a>
-    //     </li>
-    //     <li class={`border-b-2 ${active("/login")} mx-1.5 sm:mx-6`}>
-    //       <a href="/login">Login</a>
-    //     </li>
-    //   </ul>
-    // </nav>
+          <NavigationMenuContent class="grid w-[90vw] grid-rows-3 gap-3 sm:w-[500px] sm:grid-cols-2 md:w-[500px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] [&>li:first-child]:row-span-3">
+            <NavigationMenuLink
+              class="box-border flex size-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline focus:shadow-md"
+              href="https://solid-ui.com"
+            >
+              <NavigationMenuLabel class="mb-2 mt-4 text-lg font-medium">
+                Lorem.
+              </NavigationMenuLabel>
+              <NavigationMenuDescription class="text-sm leading-tight text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Accusamus ratione consectetur blanditiis incidunt ducimus cum.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink href="/docs">
+              <NavigationMenuLabel>Lorem, ipsum.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+                sint illo nam!
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink href="/docs/installation/overview">
+              <NavigationMenuLabel>Lorem, ipsum.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem, ipsum dolor sit amet consectetur adipisicing.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink href="/docs/dark-mode/overview">
+              <NavigationMenuLabel>Lorem.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum dolor sit amet.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            Overview
+            <NavigationMenuIcon />
+          </NavigationMenuTrigger>
+
+          <NavigationMenuContent class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+            <NavigationMenuLink href="#">
+              <NavigationMenuLabel>Introduction</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum dolor sit amet.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink href="#">
+              <NavigationMenuLabel>Getting started</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum dolor sit amet consectetur.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+            <NavigationMenuLink href="#">
+              <NavigationMenuLabel>Lorem, ipsum.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum, dolor sit amet consectetur adipisicing.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+            <NavigationMenuLink href="#">
+              <NavigationMenuLabel>Lorem.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum dolor sit amet consectetur.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+            <NavigationMenuLink href="#">
+              <NavigationMenuLabel>Lorem.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem, ipsum dolor.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+            <NavigationMenuLink href="#">
+              <NavigationMenuLabel>Lorem, ipsum.</NavigationMenuLabel>
+              <NavigationMenuDescription>
+                Lorem ipsum dolor sit.
+              </NavigationMenuDescription>
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenu>
+      <UserNav />
+    </div>
+  );
+}
+
+function UserNav() {
+  const settingsQuery = useSettingsQuery();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        as={Button<"button">}
+        variant="ghost"
+        class="relative h-8 w-8 rounded-full"
+      >
+        <Avatar class="h-8 w-8">
+          <Suspense>
+            <Show
+              when={settingsQuery.data?.imageUrl}
+              fallback={
+                <AvatarFallback>
+                  {settingsQuery.data?.firstName?.[0].toUpperCase() ?? "F"}
+                  {settingsQuery.data?.lastName?.[0].toUpperCase() ?? "L"}
+                </AvatarFallback>
+              }
+            >
+              {(url) => <AvatarImage alt="User avatar" src={url().href} />}
+            </Show>
+          </Suspense>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent class="w-56">
+        <DropdownMenuLabel class="font-normal">
+          <div class="flex flex-col space-y-1">
+            <p class="text-sm font-medium leading-none">
+              <Suspense>
+                {settingsQuery.data?.firstName} {settingsQuery.data?.lastName}
+              </Suspense>
+            </p>
+            <p class="text-xs leading-none text-muted-foreground">
+              <Suspense>{settingsQuery.data?.email}</Suspense>
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem as={A} href="/profile">
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem as={A} href="/settings">
+          Settings
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem as={A} href="/logout">
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
